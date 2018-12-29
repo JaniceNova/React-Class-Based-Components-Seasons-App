53b718e8193d1b8d28b9ad672930abd8517ba7c8
Janice novacek <janicenovacek@Janices-MacBook-Pro.local>
2018-12-29 11:22:43 -0500

    working on geolocation API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const  App = () => {
 
-    window.navigator.geolocation.getCurrentPosition(
-        (position)=> console.log(position),
-        (err)=> console.log(err)
-    )
-    return(  
-        <div> Latitude: 
-
-        </div>
-
-    );
-};
+class App extends React.Component {
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=> console.log(position),
+            (err)=> console.log(err)
+        );
+        return( 
+        <div> Latitude: </div>
+   ) 
+}
+}
 
 ReactDOM.render(
     <App />,
