34b47552d4c11b7880e7f3c83e8531504f46765c
Janice novacek <janicenovacek@Janices-MacBook-Pro.local>
2018-12-29 11:42:18 -0500

    set.State with lat

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-    render(){
+   //anytime we update our state object our render method runs
+   
+    constructor(props){
+        //super says that we want to keep the react.Component constructor stuff
+        super(props);
+// this is the only time we do direct assignment to this.state
+        this.state = {lat: null };
+    
         window.navigator.geolocation.getCurrentPosition(
-            (position)=> console.log(position),
+            (position)=> {
+                this.setState({lat : position.coords.latitude});
+            },
             (err)=> console.log(err)
-        );
+        ); 
+    }
+    //react says we have to define reder!!
+    //render method gets called all the dang time
+    render(){
+       
         return( 
-        <div> Latitude: </div>
+        <div> Latitude: {this.state.lat} </div>
    ) 
 }
 }
