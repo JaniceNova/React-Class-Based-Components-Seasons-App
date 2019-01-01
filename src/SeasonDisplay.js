2ca59833e11378c6c90a9c1d16854d8b7d2d351f
Janice novacek <janicenovacek@janices-mbp.attlocal.net>
2019-01-01 09:38:58 -0800

    added in ternary expression

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+ if (month>2 && month<9){
+    return lat>0 ? 'summer':'winter'; 
 
-const SeasonDisplay = () => {
+ }
+ else {
+     if(a){
+
+     }
+     else{
+
+     }
+ }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    console.log(props.lat);
     return(
+      
         <div>Season Display</div>
     );
 
--- src/index.js
+++ src/index.js
@@ -1,35 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
    //anytime we update our state object our render method runs
    
-    constructor(props){
-        //super says that we want to keep the react.Component constructor stuff
-        super(props);
-// this is the only time we do direct assignment to this.state
-        this.state = {lat: null, errorMessage: '' };
-    
-        window.navigator. geolocation.getCurrentPosition(
-            (position)=> {
-                this.setState({lat : position.coords.latitude});
-            },
-            (err)=> {
-                 this.setState({errorMessage: err.message});
-                }
+ 
+    state = { lat: null, errorMessage: ''};
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=> this.setState({lat : position.coords.latitude}),
+            (err)=> this.setState({errorMessage: err.message})
         ); 
+      
+    }
+
+    componentDidUpdate(){
+        console.log('My component was just updated -it reredered!');
+
     }
     //react says we have to define reder!!
     //render method gets called all the dang time
     render(){
-       
+       if(this.state.errorMessage &&!this.state.lat){
         return( 
-        <div> Latitude: {this.state.lat}
-        <br />
+        <div>
         Error: {this.state.errorMessage}
-         </div>
-   );
+        </div>
+        );
+       }
+       else if (this.state.lat && !this.state.errorMessage){
+        return( 
+          <SeasonDisplay lat={this.state.lat} />
+
+        );
+       }
+       else {
+        return( 
+           <div>Loading...</div>
+        );
+       }
+       
 }
 }
 
